ef7d994326b8cc6898e82435fe09856e0ce5ecae
Alexander Krause <akr@informatik.uni-kiel.de>
2020-07-18 11:03:55 +0200

    use specific kind-cluster

--- .development/deploy-kind.sh
+++ .development/deploy-kind.sh
@@ -4,6 +4,6 @@
 
 docker build -f src/main/docker/Dockerfile.jvm -t explorviz/trace-service-jvm .
 
-kind load docker-image explorviz/trace-service-jvm:latest
+kind load docker-image --name explorviz-dev explorviz/trace-service-jvm:latest
 
-kubectl apply --namespace=explorviz-dev -f manifest.yml
+kubectl apply --context kind-explorviz-dev -f manifest.yml
